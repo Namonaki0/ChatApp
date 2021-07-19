3c01f80d52211a9004a8f0be6c0bdb2247f118d1
Andre <andreferreira@live.co.uk>
2021-07-19 23:09:17 +0100

    first commit

---
+++ form.js
@@ -0,0 +1,28 @@
+const messageWrapper = document.querySelector("#chat-message-wrapper");
+const messageInput = document.querySelector("#message");
+const roomInput = document.querySelector("#room");
+const form = document.querySelector("#form");
+const joinRoomBtn = document.querySelector("#join-room-btn");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = messageInput.value;
+  const room = roomInput.value;
+
+  if (message === "") return;
+
+  sendMessage(message);
+
+  messageInput.value = "";
+});
+
+joinRoomBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+
+function sendMessage(message) {
+  const messageDiv = document.createElement("div");
+  messageDiv.textContent = message;
+  messageWrapper.appendChild(messageDiv);
+}
