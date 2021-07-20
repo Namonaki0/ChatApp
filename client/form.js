99a8b9f2d73f8001c2887e78c2f4f092115b4442
Andre <andreferreira@live.co.uk>
2021-07-20 23:07:37 +0100

    snowpack

--- client/form.js
+++ client/form.js
@@ -1,8 +1,12 @@
+// import { socketIo } from "socket.io-client";
+
 const messageWrapper = document.querySelector("#chat-message-wrapper");
 const messageInput = document.querySelector("#message");
 const roomInput = document.querySelector("#room");
 const form = document.querySelector("#form");
 const joinRoomBtn = document.querySelector("#join-room-btn");
+
+// const socket = socketIo("http://localhost:5500");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
