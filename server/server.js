3c438c6ec2bbbdb62e3ae58815007bbf541696e5
Andre <andreferreira@live.co.uk>
2021-07-20 23:22:40 +0100

    socket.io

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("CONNECTED");
+const socketIo = require("socket.io")(5500, {
+  cors: {
+    origin: ["http://localhost:8080"],
+  },
+});
+
+socketIo.on("connection", (socket) => {
+  console.log(socket.id);
+});
